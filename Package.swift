1b94b6cb6e6c9c8639256f27b6e86955891e0cbf
Rodrigo Kreutz <rodrigo.kreutz@acad.pucrs.br>
2018-07-09 18:46:03 -0300

    Adding MongoKitten dependency

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ let package = Package(
     dependencies: [
         // MongoDB driver for Fluent
         .Package(url: "https://github.com/vapor/mongo-driver.git", majorVersion: 2),
+        .Package(url: "https://github.com/rkreutz/MongoKitten.git", majorVersion: 4, minor: 1),
         // A provider for including Fluent in Vapor applications
         .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
         // A web framework and server for Swift that works on macOS and Ubuntu.
