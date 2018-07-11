8a8762f71cba6030cb49e4dde5f455bbad2f5aab
Rodrigo Kreutz <rodrigo.kreutz@acad.pucrs.br>
2018-07-10 23:18:47 -0300

    Adjusting dependencies

--- Package.swift
+++ Package.swift
@@ -4,8 +4,7 @@ let package = Package(
     name: "MongoProvider",
     dependencies: [
         // MongoDB driver for Fluent
-        .Package(url: "https://github.com/vapor/mongo-driver.git", majorVersion: 2),
-        .Package(url: "https://github.com/rkreutz/MongoKitten.git", majorVersion: 4, minor: 1),
+        .Package(url: "https://github.com/rkreutz/mongo-driver.git", majorVersion: 2, minor: 2),
         // A provider for including Fluent in Vapor applications
         .Package(url: "https://github.com/vapor/fluent-provider.git", majorVersion: 1),
         // A web framework and server for Swift that works on macOS and Ubuntu.
